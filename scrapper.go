077d12ee7b42f54799bf80bd189bb411584ce5d2
darrelchang03 <darrelchang@gmail.com>
2024-11-29 00:13:14 -0800

    fixed scrapper

--- scrapper.go
+++ scrapper.go
@@ -5,145 +5,107 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+
 	"github.com/gocolly/colly"
 )
 
-type PlayerStats struct {
-	// name string
-	gamesPlayed string
-	points string
-	fieldGoalsMade string
-	fieldGoalAttemps string
-	fieldGoalPct string
-	threePointsMade string
-	threePointPct string
-	freeThrowsMade string
-	freeThrowPct string
-	offensiveRebounds string
-	defensiveRebounds string
-	assists string
-	turnovers string
-	steals string
-	blocks string
-	personalFouls string
-	fantasyPoints string
-	doubleDoubles string
-	tripleDoubles string
-	plusMinus string
+type Player struct {
+	FirstName      string
+	LastName       string
+	Team           string
+	PointsPerGame  string
+	ReboundsPerGame string
+	AssistsPerGame string
+}
+
+// function to write players data to CSV
+func writeToCSV(players []Player, filename string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Fatalf("Could not create CSV file: %v", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// write CSV header
+	writer.Write([]string{"First Name", "Last Name", "Team", "PPG", "RPG", "APG"})
+
+	// write player data
+	for _, player := range players {
+		writer.Write([]string{
+			player.FirstName,
+			player.LastName,
+			player.Team,
+			player.PointsPerGame,
+			player.ReboundsPerGame,
+			player.AssistsPerGame,
+		})
+	}
 }
 
 func main() {
+	players := []Player{}
 
-	c := colly.NewCollector(
-		colly.AllowedDomains("www.nba.com"),
-	)
+	c := colly.NewCollector()
 
-	var players []PlayerStats
+	// temporary player instance
+	var currentPlayer Player
 
-	// c.Visit("https://www.nba.com/players")
-	c.Visit("https://www.nba.com/stats/player/1630163")
-	
-	c.OnHTML("a.Anchor_anchor__cSc3P.RosterRow_playerLink__qw1vG", func(e *colly.HTMLElement) {
-		playerLink := e.Attr("href")
-		fullUrl := "www.nba.com" + playerLink
-		fmt.Println("Player Link: ", fullUrl)
+	// extract player team, first name, and last name
+	c.OnHTML("div.PlayerSummary_mainInnerBio__JQkoj", func(e *colly.HTMLElement) {
+		teamText := strings.TrimSpace(e.ChildText("p.PlayerSummary_mainInnerInfo__jv3LO"))
+		teamParts := strings.Fields(teamText)
+		if len(teamParts) >= 2 {
+			currentPlayer.Team = teamParts[0] + " " + teamParts[1]
+		} else {
+			currentPlayer.Team = teamText
+		}
 
-		e.Request.Visit(fullUrl)
+		currentPlayer.FirstName = strings.TrimSpace(e.ChildText("p.PlayerSummary_playerNameText___MhqC:nth-of-type(2)"))
+		currentPlayer.LastName = strings.TrimSpace(e.ChildText("p.PlayerSummary_playerNameText___MhqC:nth-of-type(3)"))
 	})
 
-	c.OnHTML("tbody.Crom_body__UYOcU", func(e *colly.HTMLElement) {
-		fmt.Println("hi")
-		if e.Index == 0 {
-			e.ForEach("tr", func(_ int, row *colly.HTMLElement) {
-				player := PlayerStats {
-					gamesPlayed: row.ChildText("td:nth-child(1)"),
-					points: row.ChildText("td:nth-child(2)"),
-					fieldGoalsMade: row.ChildText("td:nth-child(3)"),
-					fieldGoalAttemps: row.ChildText("td:nth-child(4)"),
-					fieldGoalPct: row.ChildText("td:nth-child(5)"),
-					threePointsMade: row.ChildText("td:nth-child(6)"),
-					threePointPct: row.ChildText("td:nth-child(7)"),
-					freeThrowsMade: row.ChildText("td:nth-child(8)"),
-					freeThrowPct: row.ChildText("td:nth-child(9)"),
-					offensiveRebounds: row.ChildText("td:nth-child(10)"),
-					defensiveRebounds: row.ChildText("td:nth-child(11)"),
-					assists: row.ChildText("td:nth-child(12)"),
-					turnovers: row.ChildText("td:nth-child(13)"),
-					steals: row.ChildText("td:nth-child(14)"),
-					blocks: row.ChildText("td:nth-child(15)"),
-					personalFouls: row.ChildText("td:nth-child(16)"),
-					fantasyPoints: row.ChildText("td:nth-child(17)"),
-					doubleDoubles: row.ChildText("td:nth-child(18)"),
-					tripleDoubles: row.ChildText("td:nth-child(19)"),
-					plusMinus: row.ChildText("td:nth-child(20)"),
-				}
-				players = append(players, player)
-			})
+	// extract player stats
+	c.OnHTML("div.PlayerSummary_playerStat__rmEOP", func(e *colly.HTMLElement) {
+		statKey := strings.TrimSpace(e.ChildText("p.PlayerSummary_playerStatLabel__I3TO3"))
+		statValue := strings.TrimSpace(e.ChildText("p.PlayerSummary_playerStatValue___EDg_"))
+
+		switch statKey {
+		case "PPG":
+			currentPlayer.PointsPerGame = statValue
+		case "RPG":
+			currentPlayer.ReboundsPerGame = statValue
+		case "APG":
+			currentPlayer.AssistsPerGame = statValue
 		}
 	})
 
-	// c.OnHTML("div.PlayerSummary_mainInnerBio__JQkoj", func(e *colly.HTMLElement) {
-	// 	for i := range players {
-	// 		players[i].name = e.ChildText("p.PlayerSummary_playerNameText___MhqC:nth-child(1)") + " " +
-	// 						  e.ChildText("p.PlayerSummary_playerNameText___MhqC:nth-child(1)")
-	// 	}
-	// })
-
-	// c.OnHTML("button.Pagination_button__sqGoH", func(e *colly.HTMLElement) {
-	// 	nextPage := e.Request
-	// 	if nextPage != nil {
-	// 		e.Request.Visit("https://nba.com")
-	// 	}
-	// })
-
+	// once per player page, add the player to the list
 	c.OnScraped(func(r *colly.Response) {
-		file, err := os.Create("products.csv")
-
-		if err != nil {
-			log.Fatalln("Faled to create CSV file", err)
-			defer file.Close()
-		}
-
-		writer := csv.NewWriter(file)
-
-		headers := []string {
-			"Name", "Games Played", "Points", "FGM", "FGA", "FG%", "3PM", "3P%", "FTM",
-			"FT%", "OR", "DR", "AST", "TO", "STL", "BLK", "PF", "FP", "DD", "TD", "+/-",
-		}
-
-		writer.Write(headers)
-
-		for _, player := range players {
-			record := []string {
-				// player.name,
-				player.gamesPlayed,
-				player.points,
-				player.fieldGoalsMade,
-				player.fieldGoalAttemps,
-				player.fieldGoalPct,
-				player.threePointsMade,
-				player.threePointPct,
-				player.freeThrowsMade,
-				player.freeThrowPct,
-				player.offensiveRebounds,
-				player.defensiveRebounds,
-				player.assists,
-				player.turnovers,
-				player.steals,
-				player.blocks,
-				player.personalFouls,
-				player.fantasyPoints,
-				player.doubleDoubles,
-				player.tripleDoubles,
-				player.plusMinus,
-			}
-			writer.Write(record)
-		}
-		defer writer.Flush()
-	})	
-
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println("Request failed with status: ", r, "Error: ", err)
+		players = append(players, currentPlayer)
+		currentPlayer = Player{} // Reset for next player
 	})
 
+	// visit player URLs
+	playerURLs := []string{
+		"https://www.nba.com/stats/player/1630163",
+		"https://www.nba.com/player/201939/stephen-curry",
+		"https://www.nba.com/player/203110/draymond-green",
+		"https://www.nba.com/player/1627741/buddy-hield",
+		"https://www.nba.com/player/1626172/kevon-looney",
+		"https://www.nba.com/player/1627780/gary-payton-ii",
+		"https://www.nba.com/player/203952/andrew-wiggins",
+		"https://www.nba.com/player/203937/kyle-anderson",
+	}
+
+	for _, url := range playerURLs {
+		c.Visit(url)
+	}
+
+	// save players to CSV
+	writeToCSV(players, "players_stats.csv")
+	fmt.Println("Player data successfully written to players_stats.csv")
 }
